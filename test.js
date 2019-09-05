845b7c8369564334129e39b6c87bd3033ebee505
Diana Janstevica <dianajanstevica@MacBook-Pro-2.local>
2019-09-05 09:40:55 +0200

    p now changes color when clicked

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ document.getElementById("btn").addEventListener("click", myFunction);
 function myFunction() {
   document.getElementById("test").innerHTML = "Hello World";
 }
+
+document.getElementById("btn").onclick = function(){
+	document.getElementById("test").style.color = 'green';
+}
